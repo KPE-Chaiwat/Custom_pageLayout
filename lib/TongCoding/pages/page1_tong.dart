bd8116ef93858b3184363c159c8f221d8d3007ff
Good Boy <Lawliet10478@gmail.com>
2022-04-12 15:21:55 +0700

    takeDataObject in table

--- lib/TongCoding/pages/page1_tong.dart
+++ lib/TongCoding/pages/page1_tong.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:tpk_login_v2/TongCoding/Data/DataTest_Page1.dart';
 import 'package:tpk_login_v2/TongCoding/pages/fectors_page1/Button_custom.dart';
 import 'package:tpk_login_v2/TongCoding/pages/fectors_page1/Items_Show/CenterREv.dart';
 import 'package:tpk_login_v2/TongCoding/pages/fectors_page1/Items_Show/Heating_Temp.dart';
@@ -55,49 +56,85 @@ class Lefe_Items extends StatelessWidget {
             right_msg: "Meses",
             font_Weight: FontWeight.bold,
           ),
-          const Show_voltage_power(
+          Show_voltage_power(
             left_msg: '1st Output Voltage (v)',
             left_msg_subtitle: "Min/Max",
-            center_msg: "290",
-            right_msg: ['287', '287', '289'],
+            center_msg: dataInTable["Data001"],
+            right_msg: [
+              dataInTable["Data002"].toString(),
+              dataInTable["Data003"].toString(),
+              dataInTable["Data004"].toString(),
+            ],
           ),
-          const Show_voltage_power(
+          Show_voltage_power(
             left_msg: '1st Output Power (kw)',
             left_msg_subtitle: "Min/Max",
-            right_msg: ['9.6', '9.4', '9.6'],
+            center_msg: dataInTable["Data005"],
+            right_msg: [
+              dataInTable["Data006"].toString(),
+              dataInTable["Data007"].toString(),
+              dataInTable["Data008"].toString()
+            ],
           ),
-          const Show_voltage_power(
+          Show_voltage_power(
             left_msg: '2nd  Output Voltage (v)',
             left_msg_subtitle: "Min/Max",
-            center_msg: "185",
-            right_msg: ['183', '183', '184'],
+            center_msg: dataInTable["Data009"],
+            right_msg: [
+              dataInTable["Data010"].toString(),
+              dataInTable["Data011"].toString(),
+              dataInTable["Data012"].toString()
+            ],
           ),
-          const Show_voltage_power(
+          Show_voltage_power(
             left_msg: '2nd Output Power (kw)',
             left_msg_subtitle: "Min/Max",
-            right_msg: ['3.6', '3.5', '3.6'],
+            center_msg: dataInTable["Data013"],
+            right_msg: [
+              dataInTable["Data014"].toString(),
+              dataInTable["Data015"].toString(),
+              dataInTable["Data016"].toString(),
+            ],
           ),
-          const Show_voltage_power(
+          Show_voltage_power(
             left_msg: '3rd Output Voltage (v)',
             left_msg_subtitle: "Min/Max",
-            center_msg: "290",
-            right_msg: ['287', '287', '289'],
+            center_msg: dataInTable["Data017"],
+            right_msg: [
+              dataInTable["Data018"].toString(),
+              dataInTable["Data019"].toString(),
+              dataInTable["Data020"].toString()
+            ],
           ),
-          const Show_voltage_power(
+          Show_voltage_power(
             left_msg: '3rd Output Power (kw)',
             left_msg_subtitle: "Min/Max",
-            right_msg: ['2.5', '2.3', '2.5'],
+            center_msg: dataInTable["Data021"],
+            right_msg: [
+              dataInTable["Data022"].toString(),
+              dataInTable["Data023"].toString(),
+              dataInTable["Data024"].toString()
+            ],
           ),
-          const Show_voltage_power(
+          Show_voltage_power(
             left_msg: '4th Output Voltage (v)',
             left_msg_subtitle: "Min/Max",
-            center_msg: "0",
-            right_msg: ['0', '0', '0'],
+            center_msg: dataInTable["Data025"],
+            right_msg: [
+              dataInTable["Data026"].toString(),
+              dataInTable["Data027"].toString(),
+              dataInTable["Data028"].toString()
+            ],
           ),
-          const Show_voltage_power(
+          Show_voltage_power(
             left_msg: '4th Output Power (kw)',
             left_msg_subtitle: "Min/Max",
-            right_msg: ['0', '0', '0'],
+            center_msg: dataInTable["Data029"],
+            right_msg: [
+              dataInTable["Data030"].toString(),
+              dataInTable["Data031"].toString(),
+              dataInTable["Data032"].toString()
+            ],
           ),
           //----------------------------------------------------------------
           ShowItems(
@@ -109,29 +146,29 @@ class Lefe_Items extends StatelessWidget {
           ),
           Heating_temp(
             left_msg: " 1st HEATING TEMP.(c)",
-            center_msg: "374.",
-            right_msg: "64/374",
+            center_msg: dataInTable["Data033"],
+            right_msg: "${dataInTable["Data034"]}/${dataInTable["Data035"]}",
             font_Weight: FontWeight.normal,
             font_Size: 14,
           ),
           Heating_temp(
             left_msg: "2nd HEATING TEMP.(c)",
-            center_msg: "592.",
-            right_msg: "441/592",
+            center_msg: dataInTable["Data036"],
+            right_msg: "${dataInTable["Data037"]}/${dataInTable["Data038"]}",
             font_Weight: FontWeight.normal,
             font_Size: 14,
           ),
           Heating_temp(
             left_msg: "3rd HEATING TEMP.(c)",
-            center_msg: "683.",
-            right_msg: "64/683",
+            center_msg: dataInTable["Data039"],
+            right_msg: "${dataInTable["Data040"]}/${dataInTable["Data041"]}",
             font_Weight: FontWeight.normal,
             font_Size: 14,
           ),
           Heating_temp(
             left_msg: "4th HEATING TEMP.(c)",
-            center_msg: "0.",
-            right_msg: "0/0",
+            center_msg: dataInTable["Data042"],
+            right_msg: "${dataInTable["Data043"]}/${dataInTable["Data044"]}",
             font_Weight: FontWeight.normal,
             font_Size: 14,
           ),
@@ -157,90 +194,90 @@ class Right_Items extends StatelessWidget {
           ),
           Heating_time(
             left_msg: "1st HEATING TIME (sec)",
-            center_msg: "9.00",
-            right_msg: "9.00",
+            center_msg: dataInTable["Data045"],
+            right_msg: dataInTable["Data046"],
             font_Size: 14,
           ),
           Heating_time(
             left_msg: "2nd HEATING WAIT TIME (sec)",
-            center_msg: "0.00",
-            right_msg: "0.00",
+            center_msg: dataInTable["Data047"],
+            right_msg: dataInTable["Data048"],
             font_Size: 14,
           ),
           Heating_time(
             left_msg: "2nd HEATING TIME (sec)",
-            center_msg: "7.80",
-            right_msg: "7.80",
+            center_msg: dataInTable["Data049"],
+            right_msg: dataInTable["Data050"],
             font_Size: 14,
           ),
           Heating_time(
             left_msg: "3rd HEATING WAIT TIME (sec)",
-            center_msg: "0.00",
-            right_msg: "0.00",
+            center_msg: dataInTable["Data051"],
+            right_msg: dataInTable["Data052"],
             font_Size: 14,
           ),
           Heating_time(
             left_msg: "3rd HEATING TIME (sec)",
-            center_msg: "13.00",
-            right_msg: "13.00",
+            center_msg: dataInTable["Data053"],
+            right_msg: dataInTable["Data054"],
             font_Size: 14,
           ),
           Heating_time(
             left_msg: "4th HEATING WAIT TIME (sec)",
-            center_msg: "0.00",
-            right_msg: "0.00",
+            center_msg: dataInTable["Data055"],
+            right_msg: dataInTable["Data056"],
             font_Size: 14,
           ),
           Heating_time(
             left_msg: "4th HEATING TIME (sec)",
-            center_msg: "0.00",
-            right_msg: "0.00",
+            center_msg: dataInTable["Data057"],
+            right_msg: dataInTable["Data058"],
             font_Size: 14,
           ),
           Heating_time(
             left_msg: "AIR COOLING (sec)",
-            center_msg: "0.00",
-            right_msg: "0.00",
+            center_msg: dataInTable["Data059"],
+            right_msg: dataInTable["Data060"],
             font_Size: 14,
           ),
           ShowItems(),
           ShowItems(),
           POS(
             left_msg: "CENTERING POS. (mm)",
-            center_msg: "119.00",
-            right_msg: "119.00",
+            center_msg: dataInTable["Data061"],
+            right_msg: dataInTable["Data062"],
             font_Size: 14,
           ),
           POS(
             left_msg: "HEATING POS. (mm)",
-            center_msg: "218.90",
-            right_msg: "218.90",
+            center_msg: dataInTable["Data063"],
+            right_msg: dataInTable["Data064"],
             font_Size: 14,
           ),
           POS(
             left_msg: "HOME POS. (mm)",
-            center_msg: "100.00",
-            right_msg: "99.99",
+            center_msg: dataInTable["Data065"],
+            right_msg: dataInTable["Data066"],
             font_Size: 14,
           ),
           ShowItems(),
           ShowItems(),
           Center_REV(
             left_msg: "HEAD CENTER RREV.(rmp)",
-            center_msg: "",
-            right_msg: "105",
+            center_msg: dataInTable["Data067"],
+            right_msg: dataInTable["Data068"],
             font_Size: 14,
           ),
           Center_REV(
             left_msg: "TAIL CENTER RREV.(rmp)",
-            center_msg: "103",
-            right_msg: "",
+            center_msg: dataInTable["Data069"],
+            right_msg: dataInTable["Data070"],
             font_Size: 14,
           ),
           Center_REV(
             left_msg: "CYCLE TIME (sec)",
-            center_msg: "",
-            right_msg: "46.6",
+            center_msg: dataInTable["Data071"],
+            right_msg: dataInTable["Data072"],
             font_Size: 14,
           ),
           CustomButton()
